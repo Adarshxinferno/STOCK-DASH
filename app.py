f4794bcabecb688e21037901df3ccd32a9c09b0c
Adarsh A Nair <adarshnair210@gmail.com>
2021-08-11 14:18:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,199 @@
+from typing import Container
+import dash
+from dash.dependencies import Input, Output, State  
+from dash.exceptions import PreventUpdate
+import dash_core_components as dcc
+import dash_html_components as html
+from datetime import date, datetime as dt
+from dash_html_components.Img import Img 
+import yfinance as yf 
+import pandas as pd 
+import plotly.graph_objs as go
+import plotly.express as px 
+from model import prediction
+from sklearn.svm import SVR
+import dash_extensions as de
+
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+def get_stock_price_fig(df):
+
+    fig = px.line(df,
+                  x="Date",
+                  y=["Close", "Open"],
+                  title="Closing and Openning Price vs Date")
+
+    fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+    return fig
+
+
+def get_more(df):
+    df['EWA_20'] = df['Close'].ewm(span=20, adjust=False).mean()
+    fig = px.scatter(df,
+                     x="Date",
+                     y="EWA_20",
+                     title="Exponential Moving Average vs Date")
+    fig.update_traces(mode='lines+markers')
+    fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+    return fig
+
+
+
+app = dash.Dash(__name__)
+server = app.server
+options = dict(loop=True, autoplay=True,rendererSettings=dict(preserveAspectRatio='xMidYMid slice',background='transparent'))
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                # Navigation
+                html.Img(src='assets\stockifylogo.png', style={'height':'25%', 'width':'auto', 'display':'flex'}),
+                html.P("Welcome to Stockify!", className="start"),
+                html.Div([
+                    html.P("Input stock code: "),
+                    html.Div([
+                        dcc.Input(id="dropdown_tickers", type="text"),
+                        html.Button("Submit", id='submit'),
+                    ],
+                             className="form")
+                ],
+                         className="input-place"),
+                html.Div([
+                    dcc.DatePickerRange(id='my-date-picker-range',
+                                        min_date_allowed=dt(1995, 8, 5),
+                                        max_date_allowed=dt.now(),
+                                        initial_visible_month=dt.now(),
+                                        end_date=dt.now().date()),
+                ],
+                         className="date"),
+                html.Div([
+                    html.Button(
+                        "Stock Price", className="stock-btn", id="stock"),
+                    html.Button("Indicators",
+                                className="indicators-btn",
+                                id="indicators"),
+                    dcc.Input(id="n_days",
+                              type="text",
+                              placeholder="number of days"),
+                    html.Button(
+                        "Forecast", className="forecast-btn", id="forecast")
+                ],
+                         className="buttons"),
+                # here
+            ],
+            className="nav"),
+
+        # content
+        html.Div(
+            [
+                html.Div(
+                    [  # header
+                        html.Img(id="logo"),
+                        html.P(id="ticker")
+                    ],
+                    className="header"),
+                html.Div(id="description", className="decription_ticker"),
+                html.Div([], id="graphs-content"),
+                html.Div([], id="main-content"),
+                html.Div([], id="forecast-content")
+            ],
+            className="content"),
+    ],
+    className="container")
+
+@app.callback([
+    Output("description", "children"),
+    Output("logo", "src"),
+    Output("ticker", "children"),
+    Output("stock", "n_clicks"),
+    Output("indicators", "n_clicks"),
+    Output("forecast", "n_clicks"),
+], [Input("submit", "n_clicks")], [State("dropdown_tickers", "value")])
+def update_data(n, val):  # inpur parameter(s)
+    if n == None:
+        return "Hey there! Please enter a legitimate stock code to get details.",None, de.Lottie(options=options, width="110%", height="110%", url=app.get_asset_url('business_analytics.json')), None, None, None
+        # raise PreventUpdate
+    else:
+        if val == None:
+            raise PreventUpdate
+        else:
+            ticker = yf.Ticker(val)
+            inf = ticker.info
+            df = pd.DataFrame().from_dict(inf, orient="index").T
+            df[['logo_url', 'shortName', 'longBusinessSummary']]
+            return df['longBusinessSummary'].values[0], df['logo_url'].values[
+                0], df['shortName'].values[0], None, None, None
+
+@app.callback([
+    Output("graphs-content", "children"),
+], [
+    Input("stock", "n_clicks"),
+    Input('my-date-picker-range', 'start_date'),
+    Input('my-date-picker-range', 'end_date')
+], [State("dropdown_tickers", "value")])
+def stock_price(n, start_date, end_date, val):
+    if n == None:
+        return [""]
+        #raise PreventUpdate
+    if val == None:
+        raise PreventUpdate
+    else:
+        if start_date != None:
+            df = yf.download(val, str(start_date), str(end_date))
+        else:
+            df = yf.download(val)
+
+    df.reset_index(inplace=True)
+    fig = get_stock_price_fig(df)
+    return [dcc.Graph(figure=fig)]
+
+@app.callback([Output("main-content", "children")], [
+    Input("indicators", "n_clicks"),
+    Input('my-date-picker-range', 'start_date'),
+    Input('my-date-picker-range', 'end_date')
+], [State("dropdown_tickers", "value")])
+def indicators(n, start_date, end_date, val):
+    if n == None:
+        return [""]
+    if val == None:
+        return [""]
+
+    if start_date == None:
+        df_more = yf.download(val)
+    else:
+        df_more = yf.download(val, str(start_date), str(end_date))
+
+    df_more.reset_index(inplace=True)
+    fig = get_more(df_more)
+    return [dcc.Graph(figure=fig)]
+
+@app.callback([Output("forecast-content", "children")],
+              [Input("forecast", "n_clicks")],
+              [State("n_days", "value"),
+               State("dropdown_tickers", "value")])
+def forecast(n, n_days, val):
+    if n == None:
+        return [""]
+    if val == None:
+        raise PreventUpdate
+    fig = prediction(val, int(n_days) + 1)
+    return [dcc.Graph(figure=fig)]  
+
+
+
+
+if   __name__ == '__main__':
+    app.run_server(debug=True)
+
